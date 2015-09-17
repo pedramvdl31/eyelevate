d955df3459ab68e9ccb5fc72f7108daee14f18db
wondollaballa <onedough83@gmail.com>
2015-09-17 15:53:22 -0700

    update object error

--- app/Task.php
+++ app/Task.php
@@ -193,7 +193,7 @@ class Task extends Model
                 $tasks['assigned_username'] = $users->username;
             }
             if(isset($tasks['image_src'])) {
-                $tasks['image_src'] = (isset($tasks->image_src)) ? json_decode($tasks->image_src) : $tasks->image_src;
+                $tasks['image_src'] = (json_decode($tasks->image_src)) ? json_decode($tasks->image_src) : $tasks->image_src;
             }
             if(isset($tasks['status'])) {
                 $tasks['status'] = Task::prepareStatusForView($tasks->status);
@@ -204,7 +204,6 @@ class Task extends Model
             if(isset($tasks['project_id'])) {
                 $tasks['project_name'] = Project::GetProjectsName($tasks['project_id']);
             }
-            Job::dump($tasks['image_src']);
         }
         return $tasks;
     }
