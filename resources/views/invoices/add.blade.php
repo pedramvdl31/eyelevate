e5adcabeacabf0a829ea8ef35edbb8863eafba39
wondollaballa <onedough83@gmail.com>
2015-09-28 07:24:13 +0900

    update

--- resources/views/invoices/add.blade.php
+++ resources/views/invoices/add.blade.php
@@ -42,18 +42,95 @@
 			<h3 class="panel-title">Invoice Item Details</h3>
 		</div>		
 		<div class="panel-body">
+			<div class="row-fluid clearfix">
+			  	<div class="form-group {{ $errors->has('invoice_item.title') ? 'has-error' : false }}">
+			    	<label class="control-label" for="title">Summary</label>
+			    	{!! Form::text('invoice_item.title', null, array('class'=>'form-control', 'placeholder'=>'Invoice Item Summary')) !!}
+			        @foreach($errors->get('invoice_item.title') as $message)
+			            <span class='help-block'>{{ $message }}</span>
+			        @endforeach
+			  	</div>	
+			  	<div class="form-group {{ $errors->has('invoice_item.description') ? 'has-error' : false }}">
+			    	<label class="control-label" for="title">Short Description</label>
+			    	{!! Form::text('invoice_item.description', null, array('class'=>'form-control', 'placeholder'=>'Invoice Item Description')) !!}
+			        @foreach($errors->get('invoice_item.description') as $message)
+			            <span class='help-block'>{{ $message }}</span>
+			        @endforeach
+			  	</div>	
+			  	<div class="form-group {{ $errors->has('invoice_item.quantity') ? 'has-error' : false }}">
+			    	<label class="control-label" for="invoice_item.quantity">Quantity</label>
+			    	{!! Form::text('invoice_item.quantity', null, array('class'=>'form-control', 'placeholder'=>'Quantity')) !!}
+			        @foreach($errors->get('invoice_item.quantity') as $message)
+			            <span class='help-block'>{{ $message }}</span>
+			        @endforeach
+			  	</div>
+			  	<div class="form-group {{ $errors->has('invoice_item.subtotal') ? 'has-error' : false }}">
+			    	<label class="control-label" for="invoice_item.subtotal">Subtotal</label>
+			    	{!! Form::text('invoice_item.subtotal', null, array('class'=>'form-control', 'placeholder'=>'Sutbtotal')) !!}
+			        @foreach($errors->get('invoice_item.subtotal') as $message)
+			            <span class='help-block'>{{ $message }}</span>
+			        @endforeach
+			  	</div>		
+		  		<div class="form-group {{ $errors->has('invoice_item.tax') ? 'has-error' : false }}">
+			    	<label class="control-label" for="role_id">Tax</label>
+			    	{!! Form::select('invoice_item.tax', $projects, null ,array('class'=>'form-control')) !!}
+			        @foreach($errors->get('invoice_item.tax') as $message)
+			            <span class='help-block'>{{ $message }}</span>
+			        @endforeach
+			  	</div>	
+			</div>
+			<div class="row-fluid clearfix">
+				<button type="button" class="btn btn-info">Add Invoice Item</button>
+			</div>
+			
+		</div>
+		<div class="panel-heading" style="border-top:1px solid #ddd; border-radius:0px;">
+			<h3 class="panel-title">Invoice Summary</h3>
+		</div>		
+		<div class="panel-body">
 			<div class="table-responsive">
 				<table class="table table-hover table-condensed">
 					<thead>
 						<tr>
 							<th>Item</th>
 							<th>Description</th>
+							<th>Quantity</th>
 							<th>Subtotal</th>
 							<th>Tax</th>
 							<th>Total</th>
 							<th>Action</th>
 						</tr>
 					</thead>
+					<tbody>
+						<tr class="hide">
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>
+								<button type="button" class="btn btn-sm btn-danger">Remove</button>
+							</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr style="">
+							<th colspan="5" style="border-bottom:none;"></th>
+							<th style="text-align:right;">Total Subtotal</th>
+							<th></th>
+						</tr>
+						<tr>
+							<th colspan="5"></th>
+							<th style="text-align:right;">Total Tax</th>
+							<th></th>
+						</tr>
+						<tr>
+							<th colspan="5"></th>
+							<th style="text-align:right;">Total Due</th>
+							<th></th>
+						</tr>
+					</tfoot>
 				</table>
 
 			</div>
